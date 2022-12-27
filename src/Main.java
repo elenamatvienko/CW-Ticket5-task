bff0c355b137b71b97cceafd6e836a04f950fcc4
elena <elena.matvienko2011@mail.ru>
2022-12-27 13:42:35 +0300

    Решение задачи Билет 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    /*Дан массив пар названий книг и авторов,
+    необходимо привести каждую пару в строку вида:
+    «Название книги» И.О.Автора.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+String [] book = {"\"Вий\" Н.В. Гоголь", "\"Мгла\" С. Кинг", "\"Мать\" М. Горький"};
+        for (int i = 0; i < book.length ; i++) {
+            System.out.println(book[i]);
+        }
     }
 }
